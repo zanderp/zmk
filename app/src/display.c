a6ddbe0ac7eb1b65e87cfed2a8cf50d84e661bce
Pete Johanson <peter@peterjohanson.com>
2020-07-27 16:31:19 -0400

    Some initial logo work.

--- app/src/display.c
+++ app/src/display.c
@@ -20,10 +20,19 @@ static struct device *display;
 
 static lv_obj_t *screen;
 
+#ifdef CONFIG_ZMK_DISPLAY_LOGO
+extern const lv_img_dsc_t zmk_logo;
+#endif
+
 int zmk_display_init()
 {
-    lv_obj_t *hello_world_label;
-    lv_obj_t *count_label;
+    lv_obj_t *zmk_brand_lbl;
+    lv_obj_t *keyboard_name_lbl;
+    lv_obj_t *outer_container;
+    lv_obj_t *lbl_cont;
+#ifdef CONFIG_ZMK_DISPLAY_LOGO
+    lv_obj_t *logo_img;
+#endif 
 
     LOG_DBG("");
 
@@ -36,12 +45,26 @@ int zmk_display_init()
     screen = lv_obj_create(NULL, NULL);
     lv_scr_load(screen);
 
-    hello_world_label = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(hello_world_label, "ZMK v0.1.0");
-    lv_obj_align(hello_world_label, NULL, LV_ALIGN_CENTER, 0, 0);
-    count_label = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(count_label, CONFIG_ZMK_KEYBOARD_NAME);
-    lv_obj_align(count_label, NULL, LV_ALIGN_IN_BOTTOM_MID, 0, 0);
+    outer_container = lv_cont_create(lv_scr_act(), NULL);
+    lv_obj_set_style(outer_container, &lv_style_transp_tight);
+    lv_cont_set_layout(outer_container, LV_LAYOUT_ROW_M);
+    lv_cont_set_fit(outer_container, LV_FIT_FLOOD);
+
+#ifdef CONFIG_ZMK_DISPLAY_LOGO
+    logo_img = lv_img_create(outer_container, NULL);
+    lv_img_set_src(logo_img, &zmk_logo);
+    lv_obj_set_style(logo_img, &lv_style_transp_tight);
+#endif 
+
+    lbl_cont = lv_cont_create(outer_container, NULL);
+    lv_obj_set_style(lbl_cont, &lv_style_transp_tight);
+    lv_cont_set_layout(lbl_cont, LV_LAYOUT_COL_M);
+    lv_cont_set_fit(lbl_cont, LV_FIT_FLOOD);
+    zmk_brand_lbl = lv_label_create(lbl_cont, NULL);
+    lv_label_set_text(zmk_brand_lbl, "ZMK");
+    keyboard_name_lbl = lv_label_create(lbl_cont, NULL);
+    lv_label_set_long_mode(keyboard_name_lbl, LV_LABEL_LONG_BREAK);
+    lv_label_set_text(keyboard_name_lbl, CONFIG_ZMK_KEYBOARD_NAME);
     lv_task_handler();
     display_blanking_off(display);
 
