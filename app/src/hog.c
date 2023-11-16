a6ac99a4f5723b8973c0b8b9d35cb4fa1e26aae2
Cem Aksoylar <caksoylar@users.noreply.github.com>
2023-11-15 21:11:41 -0800

    feat(mouse): Add mouse move and scroll support [WIP]
    Co-authored-by: Alexander Krikun <krikun98@gmail.com>
    Co-authored-by: Robert U <urob@users.noreply.github.com>
    Co-authored-by: Shawn Meier <ftc@users.noreply.github.com>

--- app/src/hog.c
+++ app/src/hog.c
@@ -367,29 +367,26 @@ void send_mouse_report_callback(struct k_work *work) {
     }
 };
 
+K_WORK_DEFINE(hog_mouse_work, send_mouse_report_callback);
+
 int zmk_hog_send_mouse_report(struct zmk_hid_mouse_report_body *report) {
-    struct bt_conn *conn = destination_connection();
-    if (conn == NULL) {
-        return 1;
-    }
-
-    struct bt_gatt_notify_params notify_params = {
-        .attr = &hog_svc.attrs[13],
-        .data = report,
-        .len = sizeof(*report),
-    };
-
-    int err = bt_gatt_notify_cb(conn, &notify_params);
+    int err = k_msgq_put(&zmk_hog_mouse_msgq, report, K_NO_WAIT);
     if (err) {
-        LOG_DBG("Error notifying %d", err);
-        return err;
+        switch (err) {
+        case -EAGAIN: {
+            LOG_WRN("Mouse message queue full, dropping report");
+            return err;
+        }
+        default:
+            LOG_WRN("Failed to queue mouse report to send (%d)", err);
+            return err;
+        }
     }
 
-    bt_conn_unref(conn);
+    k_work_submit_to_queue(&hog_work_q, &hog_mouse_work);
 
     return 0;
 };
-
 #endif // IS_ENABLED(CONFIG_ZMK_MOUSE)
 
 int zmk_hog_init(void) {
