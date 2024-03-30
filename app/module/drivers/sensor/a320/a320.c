0f2385d1a0bccc462b8fff814aa325b6448570ad
Zitao <145678024+ZitaoTech@users.noreply.github.com>
2024-03-30 18:21:35 +0000

    fix trackpad bug

---
+++ app/module/drivers/sensor/a320/a320.c
@@ -0,0 +1,69 @@
+#define DT_DRV_COMPAT avago_a320
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/__assert.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/crc.h>
+#include <zephyr/logging/log.h>
+
+#include "a320.h"
+
+LOG_MODULE_REGISTER(A320, CONFIG_SENSOR_LOG_LEVEL);
+
+static int a320_read_reg(const struct device *dev, uint8_t reg_addr) {
+    const struct a320_config *cfg = dev->config;
+    uint8_t value = 0;
+
+    if (!i2c_reg_read_byte_dt(&cfg->bus, reg_addr, &value)) {
+        return value;
+    }
+
+    LOG_ERR("failed to read 0x%x register", reg_addr);
+    return 0;
+}
+
+static int a320_sample_fetch(const struct device *dev, enum sensor_channel chan) { return 0; }
+
+static int a320_channel_get(const struct device *dev, enum sensor_channel chan,
+                            struct sensor_value *val) {
+    const uint8_t ifmotion = a320_read_reg(dev, Motion);
+    const uint8_t ovflow = a320_read_reg(dev, Motion);
+    val->val1 = a320_read_reg(dev, Delta_X);
+    val->val2 = a320_read_reg(dev, Delta_Y);
+    LOG_DBG("you get the x value : %d", val->val1);
+    LOG_DBG("you get the y value : %d", val->val2);
+    return 0;
+}
+
+static const struct sensor_driver_api a320_driver_api = {
+    .sample_fetch = a320_sample_fetch,
+    .channel_get = a320_channel_get,
+};
+
+static int a320_init(const struct device *dev) {
+
+    const struct a320_config *cfg = dev->config;
+    if (!device_is_ready(cfg->bus.bus)) {
+        LOG_ERR("I2C bus %s is not ready!", cfg->bus.bus->name);
+        return -EINVAL;
+    }
+    a320_read_reg(dev, Motion);
+    a320_read_reg(dev, Motion);
+    a320_read_reg(dev, Delta_X);
+    a320_read_reg(dev, Delta_Y);
+    LOG_DBG("A320 Init done, Ready to read data.");
+
+    return 0;
+}
+
+#define A320_DEFINE(inst)                                                                          \
+    struct a320_data a3200_data_##inst;                                                            \
+    static const struct a320_config a3200_cfg_##inst = {.bus = I2C_DT_SPEC_INST_GET(inst)};        \
+    DEVICE_DT_INST_DEFINE(inst, a320_init, NULL, &a3200_data_##inst, &a3200_cfg_##inst,            \
+                          POST_KERNEL, 60, &a320_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(A320_DEFINE)
--- app/src/main.c
+++ app/src/main.c
@@ -17,20 +17,177 @@ LOG_MODULE_REGISTER(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/display.h>
 #include <drivers/ext_power.h>
 
+#include <zmk/hid.h>
+#include <dt-bindings/zmk/mouse.h>
+#include <zmk/hid_indicators.h>
+#include <zmk/indicator_capslock.h>
 #ifdef CONFIG_ZMK_MOUSE
 #include <zmk/mouse.h>
 #endif /* CONFIG_ZMK_MOUSE */
+static const struct device *get_a320_device(void) {
+    const struct device *dev = DEVICE_DT_GET_ANY(avago_a320);
 
+    if (dev == NULL) {
+        printk("\nError: no device found.\n");
+        return NULL;
+    }
+    if (!device_is_ready(dev)) {
+        printk("\nError: Device \"%s\" is not ready; "
+               "check the driver initialization logs for errors.\n",
+               dev->name);
+        return NULL;
+    }
+    printk("Found device \"%s\", getting sensor data\n", dev->name);
+    return dev;
+}
 int main(void) {
     LOG_INF("Welcome to ZMK!\n");
 
     if (zmk_kscan_init(DEVICE_DT_GET(ZMK_MATRIX_NODE_ID)) != 0) {
         return -ENOTSUP;
     }
-
+    const struct device *dev = get_a320_device();
+    if (dev == NULL) {
+        return;
+    }
 #ifdef CONFIG_ZMK_DISPLAY
     zmk_display_init();
 #endif /* CONFIG_ZMK_DISPLAY */
-
-    return 0;
+    struct sensor_value xy_pos;
+    while (1) {
+        sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &xy_pos);
+        char target_9900[] = "bb9900";
+        char target_q10[] = "bbq10";
+        char target_q20[] = "bbq20";
+        char target_q30[] = "bbq30";
+        char target_9981[] = "bb9981";
+        char target_9983[] = "bb9983";
+        if (strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_9900) == 0) {
+            int8_t x = xy_pos.val2;
+            int8_t y = xy_pos.val1;
+            int8_t scroll_x = 0;
+            int8_t scroll_y = 0;
+            x = ((x < 127) ? x : (x - 256)) * 1.5;
+            y = ((y < 127) ? y : (y - 256)) * 1.5;
+            if (zmk_hid_indicators_get_current_profile() == 2 ||
+                zmk_hid_indicators_get_current_profile() == 3 ||
+                zmk_hid_indicators_get_current_profile() == 7) {
+                if (abs(y) >= 128) {
+                    scroll_x = x / 24;
+                    scroll_y = -y / 24;
+                } else if (abs(y) >= 64 && abs(y) < 128) {
+                    scroll_x = x / 16;
+                    scroll_y = -y / 16;
+                } else if (abs(y) >= 32 && abs(y) < 64) {
+                    scroll_x = x / 12;
+                    scroll_y = -y / 12;
+                } else if (abs(y) >= 21 && abs(y) < 32) {
+                    scroll_x = x / 8;
+                    scroll_y = -y / 8;
+                } else if (abs(y) >= 3 && abs(y) < 20) {
+                    scroll_x = (x > 0) ? 1 : (x < 0) ? -1 : 0;
+                    scroll_y = -((y > 0) ? 1 : (y < 0) ? -1 : 0);
+                } else if (abs(y) >= 0 && abs(y) < 2) {
+                    scroll_x = 0;
+                    scroll_y = 0;
+                }
+                k_sleep(K_MSEC(20));
+                x = 0;
+                y = 0;
+            } else {
+                x = ((x < 127) ? x : (x - 256)) * 1.5;
+                y = ((y < 127) ? y : (y - 256)) * 1.5;
+            }
+            zmk_hid_mouse_movement_set(0, 0);
+            zmk_hid_mouse_movement_update(x, y);
+            zmk_hid_mouse_scroll_set(0, 0);
+            zmk_hid_mouse_scroll_update(scroll_x, scroll_y);
+            zmk_endpoints_send_mouse_report();
+        } else if (strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_q10) == 0 ||
+                   strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_q30) == 0 ||
+                   strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_9981) == 0 ||
+                   strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_9983) == 0) {
+            int8_t x = xy_pos.val1;
+            int8_t y = -1 * xy_pos.val2;
+            // LOG_DBG("x value : %d , y value : %d\r\n", val->val1, val->val2);
+            int8_t scroll_x = 0;
+            int8_t scroll_y = 0;
+            if (zmk_hid_indicators_get_current_profile() == 2 ||
+                zmk_hid_indicators_get_current_profile() == 3 ||
+                zmk_hid_indicators_get_current_profile() == 7) {
+                if (abs(y) >= 128) {
+                    scroll_x = x / 24;
+                    scroll_y = -y / 24;
+                } else if (abs(y) >= 64 && abs(y) < 128) {
+                    scroll_x = x / 16;
+                    scroll_y = -y / 16;
+                } else if (abs(y) >= 32 && abs(y) < 64) {
+                    scroll_x = x / 12;
+                    scroll_y = -y / 12;
+                } else if (abs(y) >= 21 && abs(y) < 32) {
+                    scroll_x = x / 8;
+                    scroll_y = -y / 8;
+                } else if (abs(y) >= 3 && abs(y) < 20) {
+                    scroll_x = (x > 0) ? 1 : (x < 0) ? -1 : 0;
+                    scroll_y = -((y > 0) ? 1 : (y < 0) ? -1 : 0);
+                } else if (abs(y) >= 0 && abs(y) < 2) {
+                    scroll_x = 0;
+                    scroll_y = 0;
+                }
+                k_sleep(K_MSEC(20));
+                x = 0;
+                y = 0;
+            } else {
+                x = ((x < 127) ? x : (x - 256)) * 1.5;
+                y = ((y < 127) ? y : (y - 256)) * 1.5;
+            }
+            zmk_hid_mouse_movement_set(0, 0);
+            zmk_hid_mouse_movement_update(x, y);
+            zmk_hid_mouse_scroll_set(0, 0);
+            zmk_hid_mouse_scroll_update(scroll_x, scroll_y);
+            zmk_endpoints_send_mouse_report();
+        } else if (strcmp(CONFIG_ZMK_KEYBOARD_NAME, target_q20) == 0) {
+            int8_t x = -1 * xy_pos.val1;
+            int8_t y = xy_pos.val2;
+            // LOG_DBG("x value : %d , y value : %d\r\n", val->val1, val->val2);
+            int8_t scroll_x = 0;
+            int8_t scroll_y = 0;
+            if (zmk_hid_indicators_get_current_profile() == 2 ||
+                zmk_hid_indicators_get_current_profile() == 3 ||
+                zmk_hid_indicators_get_current_profile() == 7) {
+                if (abs(y) >= 128) {
+                    scroll_x = x / 24;
+                    scroll_y = -y / 24;
+                } else if (abs(y) >= 64 && abs(y) < 128) {
+                    scroll_x = x / 16;
+                    scroll_y = -y / 16;
+                } else if (abs(y) >= 32 && abs(y) < 64) {
+                    scroll_x = x / 12;
+                    scroll_y = -y / 12;
+                } else if (abs(y) >= 21 && abs(y) < 32) {
+                    scroll_x = x / 8;
+                    scroll_y = -y / 8;
+                } else if (abs(y) >= 3 && abs(y) < 20) {
+                    scroll_x = (x > 0) ? 1 : (x < 0) ? -1 : 0;
+                    scroll_y = -((y > 0) ? 1 : (y < 0) ? -1 : 0);
+                } else if (abs(y) >= 0 && abs(y) < 2) {
+                    scroll_x = 0;
+                    scroll_y = 0;
+                }
+                k_sleep(K_MSEC(20));
+                x = 0;
+                y = 0;
+            } else {
+                x = ((x < 127) ? x : (x - 256)) * 1.5;
+                y = ((y < 127) ? y : (y - 256)) * 1.5;
+            }
+            zmk_hid_mouse_movement_set(0, 0);
+            zmk_hid_mouse_movement_update(x, y);
+            zmk_hid_mouse_scroll_set(0, 0);
+            zmk_hid_mouse_scroll_update(scroll_x, scroll_y);
+            zmk_endpoints_send_mouse_report();
+        }
+    }
+    int polling_ms = (1.0 / (float)CONFIG_INPUT_A320_POLLINGRATE) * 1000.0;
+    k_sleep(K_MSEC(polling_ms));
 }
